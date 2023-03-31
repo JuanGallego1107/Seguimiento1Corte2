750551dd26ff710374739ee8df4f830bb993d9b8
JuanGallego1107 <118768557+JuanGallego1107@users.noreply.github.com>
2023-03-30 20:58:56 -0500

    Delete EjerciciosEnum directory

--- EjerciciosEnum/src/Ejemplo1/Ej1Main.java
+++
@@ -1,9 +0,0 @@
-package Ejemplo1;
-
-public class Ej1Main {
-    public static void main(String[] args) {
-        for(Ejemplo1.Vehicles v : Ejemplo1.Vehicles.values()){
-            System.out.println(v);
-        }
-    }
-}
